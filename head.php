2bfb11cc8779b850d1c58dee290bddfeffd9d8a9
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-04-18 13:28:58 -0400

    new file:   accueil.html 	modified:   head.php 	modified:   index.php

--- head.php
+++ head.php
@@ -6,3 +6,47 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-brand"  onclick="gola(4)">Une société, disons... Gaz de france</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="navbar-brand" onclick="gola(1)">Articles</a>
+          </li>
+          <li class="nav-item">
+            <span class="nav-link" onclick="gola(2)">Commandes</span>
+          </li>
+          <li class="nav-item">
+            <span class="nav-link" onclick="gola(3)">Client</span>
+          </li>
+        </ul>
+        </div>
+    </nav>
+    
+    <?php
+        function gola($a) {
+            switch($a) {
+                case 1:
+                    $page='article.html';
+                    echo ('<script type="text/javascript"> document.getElementById("footer").style.backgroundColor = "red"; </script>');
+                    break;
+                case 2:
+                    $page='Commandes.html';
+                    echo ('<script type="text/javascript"> document.getElementById("footer").style.backgroundColor = "red"; </script>');
+                    break;
+                case 3:
+                    $page='Client.html';
+                    echo ('<script type="text/javascript"> document.getElementById("footer").style.backgroundColor = "red"; </script>');
+                    break;
+                case 4:
+                    $page='accueil.html';
+                    echo ('<script type="text/javascript"> document.getElementById("footer").style.backgroundColor = "red"; </script>');
+                    break;
+            }
+        } 
+    ?>
+</header>
--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<?php include 'head.php'; ?>
+<?php
+//    if (isset($_POST['page'])) { $page = $_POST['page'].'html'; }
+//    else { header 'location?page=accueil'; }*
+    $page = 'accueil';
 
-<header>
-    <div class="pos-f-t">
-      <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark p-4">
-          <h5 class="text-white h4">Collapsed content</h5>
-          <span class="text-muted">Toggleable via the navbar brand.</span>
-        </div>
-      </div>
-      <nav class="navbar navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </nav>
-    </div>
-</header>
+    include 'head.php'; 
+?>
+
+<?php include $page ;?>
+
+<?php include 'footer.html'; ?>
+

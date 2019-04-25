b913c621e76754b1c15c88454d4ad4f086e0435e
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-04-25 10:22:51 -0400

    .

--- accueil.php
+++ accueil.php
@@ -1,1 +1,15 @@
-<p>Accueil</p>
+    <div class="contain">
+        <form action="login.php" class="log">
+            <div class="form-group">
+                <label for="user">Username :</label><br>
+                <input name="user" type="text" id="user" class="form-control" >
+            </div>
+            <label for="login">Login :</label><br>
+            <input name="login" type="text" id="login" class="form-control" ><br>
+            <input type="submit"><br><br><br>
+            <a href=""><i>Mot de passe oublié</i></a><br>
+            <small id="emailHelp" class="form-text text-muted">Noter votre mot de passe sur un post-it collé sur votre bureau n'est pas sécurisé.</small>
+            <br><br>
+
+        </form>
+    </div>

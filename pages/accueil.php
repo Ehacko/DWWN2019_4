82ea98f5b22033c090972b75bee7a64cea5f8273
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2019-04-29 14:45:17 -0400

    Update accueil.php

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,5 +1,7 @@
 <?php
-    if ($_SESSION['login'][0] == 0) { include 'accueil_login.php'; }
-    else { include 'accueil_accoumpt.php'; }
+    if (isset($_SESSION['login'])) {
+        if ($_SESSION['login'][0] == 0) { include 'accueil_login.php'; }
+        else { include 'accueil_accoumpt.php'; }
+    } else { include 'accueil_login.php'; }
 ?>
   

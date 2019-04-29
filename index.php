09dd301b1944e76f5f83280aaa250a1a83a4e85e
ehacko <darkdav.T@gmail.com>
2019-04-29 10:51:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php 
     session_start();
-<<<<<<< HEAD
 
     include 'script/script.php';
 
     include 'head.php';
 
-=======
-    include 'script/script.php';
     include 'head.php';
->>>>>>> bacd577f58e72125b06239070dbc1f7168d3520c
     include 'header.php' ;
     include $page;
     include 'footer.php';

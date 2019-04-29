4032cd00d9487c731fc809544af2ceedb12059ff
ehacko <darkdav.T@gmail.com>
2019-04-29 10:44:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php 
     session_start();
+<<<<<<< HEAD
 
     include 'script/script.php';
 
     include 'head.php';
 
+=======
+    include 'script/script.php';
+    include 'head.php';
+>>>>>>> bacd577f58e72125b06239070dbc1f7168d3520c
     include 'header.php' ;
     include $page;
     include 'footer.php';
